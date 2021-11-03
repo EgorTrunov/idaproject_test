d099d7fd40f01decb9066bad7e1b0bed76a1f70b
EgorTrunov <88332602+EgorTrunov@users.noreply.github.com>
2021-11-03 21:52:35 +0300

    start layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    <h1>Добавление товара</h1>
-    <ProductList />
+  <div class="container">
+      <ProductList />
   </div>
 </template>
 
@@ -17,12 +16,28 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  $bg-color: #fffefb80;
+  $text-color: #3f3f3f;
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font: {
+    family: 'Source Sans Pro', sans-serif;
+    size: 12px;
+    weight: 400;
+  }
+  color: $text-color;
+  background: $bg-color;
+}
+
+.container {
+  max-width: 1360px;
+  margin: 0 auto;
+  padding: 32px 5px 0;
+  display: grid;
+  grid-template-columns: 4fr 12fr;
+  gap: 16px;
 }
 </style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="hello">
-    
+  <div class="">
+    <h1 class="title">Добавление товара</h1>
+    <form class="form">
+      <label for="name">
+        <input class="form__input" type="text" name="name">
+      </label>
+    </form>
   </div>
 </template>
 
@@ -11,5 +16,34 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$bg-color: #fffefb;
+$text-color: #3f3f3f;
+$important-color: #ff8484;
+$disabled-color: #eeeeee;
+$bx-shodow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
 
+.title {
+  font: {
+    weight: 600;
+    size: 28px;
+  }
+  line-height: 35px;
+  margin-bottom: 16px;
+}
+
+%input {
+  width: 100%;
+}
+
+.form {
+  padding: 24px;
+  border-radius: 4px;
+  background: $bg-color;
+  box-shadow: $bx-shodow;
+  max-width: 320px;
+  .form__input {
+    @extend %input;
+  }
+
+}
 </style>
